e185870294db178a71ae9764b21971f3bb84fb93
Namkhaidorj Batbaatar <mglcoc102@gmail.com>
2024-01-02 18:50:34 +0800

    w-changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <div class="w-screen h-fit flex flex-col pt-4 pr-20 pl-20">
+      <div class="w-secreen xl:w-screen h-fit flex flex-col pt-4 pr-20 pl-20">
         <Header />
         <Main />
         <MainText />
